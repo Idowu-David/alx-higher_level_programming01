5728d71693119a1cf642cb21907ab5c8fe158e16
Idowu-David <idowudavidodun@gmail.com>
2023-01-06 00:26:11 +0100

    hidden

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    name = dir(hidden_4)
+    sort_name = sorted[arg for i in name if i[:1] != "__"]
+    for arg in sort_name:
+        print(arg)
