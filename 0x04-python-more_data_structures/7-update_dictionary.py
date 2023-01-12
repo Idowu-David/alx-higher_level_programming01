0b449930127af29b9dd505cadaa60943655588c5
Idowu-David <idowudavidodun@gmail.com>
2023-01-12 01:17:09 +0100

    fix: update dict

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    if not a_dictionary:
-        return
-    new_dict = a_dictionary.copy()
-    for ele in new_dict.keys():
-        if ele == key:
-            a_dictionary[key] = value
-    if key not in new_dict.keys():
-        a_dictionary[key] = value
+    a_dictionary[key] = value
     return a_dictionary
