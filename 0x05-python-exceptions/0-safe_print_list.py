d4aefa3e177ee15d1b467e274e3107dc8e86034e
root <root@localhost.localdomain>
2023-01-23 09:39:28 +0000

    add: print list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    for val in my_list:
+        try:
+            if i != x:
+                print("{}".format(my_list[i]), end="")
+                i += 1
+        except IndexError:
+            break
+    print()
+    return i
+
