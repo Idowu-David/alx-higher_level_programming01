7adacab476232a706621e83f894e261352f737f4
Idowu-David <idowudavidodun@gmail.com>
2023-01-24 14:38:52 +0000

    add: square class with size/l

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Square Class """
+
+
+class Square:
+    """ defines a square class """
+    def __init__(self, size):
+        """ initializer """
+        self.__size = size
