87939bb9679f18e409cc50ef81cb3bb8ebca417b
Idowu-David <idowudavidodun@gmail.com>
2023-01-24 23:06:42 +0000

    add: print square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" Square Class """
+
+
+class Square:
+    """ defines a square class """
+    def __init__(self, size=0):
+        """ initializer """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """ size getter """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ size setter """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """ returns the current square area """
+        return self.size * self.size
+
+    def my_print(self):
+        """ prints the square with the character # """
+        if self.size == 0:
+            print()
+        sq = self.size
+        for row in range(sq):
+            for col in range(sq):
+                print("#", end="")
+            print()
