d7e1cac898e88e0120e99dda48065fa95287be10
Idowu-David <idowudavidodun@gmail.com>
2023-02-06 15:24:54 +0000

    add: print_sorted list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" print_sorted list module """
+
+
+class MyList(list):
+    """ defines MyList class """
+    def print_sorted(self):
+        """ prints the list object in a sorted order """
+        new_list = sorted(self)
+        print(new_list)
+
+
