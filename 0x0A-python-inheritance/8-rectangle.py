61589e3a7f177ec622798def1079cbc64c2dba94
Idowu-David <idowudavidodun@gmail.com>
2023-02-06 21:34:22 +0000

    add: integer validator

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -11,7 +11,7 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """ validates the `value` passed by the user """
         self.name = name
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(self.name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(self.name))
@@ -24,4 +24,5 @@ class Rectangle(BaseGeometry):
         """ initialisation """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
+        self.__width = width
         self.__height = height
