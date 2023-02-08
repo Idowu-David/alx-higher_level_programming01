31e0a03f533edf8a8d65fa01ceec5a8557aca142
Idowu-David <idowudavidodun@gmail.com>
2023-02-08 09:19:07 +0000

    add: load from json file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,6 +5,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """ writes an Object to a text file, using a JSON representation: """
-    dump = json.dumps(my_obj)
     with open(filename, "w") as f:
-        f.write(dump)
+        f.write(json.dumps(my_obj))
