9f61f77ea916ba9af0b6889c47d5cc66aba7139d
Idowu-David <idowudavidodun@gmail.com>
2023-02-09 00:20:11 +0000

    fix: added validations

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -5,8 +5,15 @@
 class Student:
     """ Student class """
     def __init__(self, first_name, last_name, age):
+        """ instance initializer """
+        if type(first_name) is not str:
+            raise ValueError("first_name must be a string")
         self.first_name = first_name
-        self.last_namr = last_name
+        if type(last_name) is not str:
+            raise ValueError("last_name must be a string")
+        self.last_name = last_name
+        if age < 0:
+            raise ValueError("age cannot be less than 0")
         self.age = age
 
     def to_json(self):
