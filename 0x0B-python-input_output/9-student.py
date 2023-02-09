dac21e30d395b67c1afe7ca3f41e8cd1764512a6
Idowu-David <idowudavidodun@gmail.com>
2023-02-09 00:27:54 +0000

    fix: lenght of name

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -8,9 +8,13 @@ class Student:
         """ instance initializer """
         if type(first_name) is not str:
             raise TypeError("first_name must be a string")
+        if len(first_name) > 10:
+            raise ValueError
         self.first_name = first_name
         if type(last_name) is not str:
             raise TypeError("last_name must be a string")
+        if len(last_name) > 10:
+            raise ValueError
         self.last_name = last_name
         if age < 0:
             raise ValueError("age cannot be less than 0")
