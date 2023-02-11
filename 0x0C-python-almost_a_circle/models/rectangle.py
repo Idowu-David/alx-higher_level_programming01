8c7bb2e32650ad0b50e10ccfd2a83db80299cfea
Idowu-David <idowudavidodun@gmail.com>
2023-02-11 03:21:01 +0000

    added few more tests

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,8 +6,64 @@ from models.base import Base
 class Rectangle(Base):
     """ defines a Rectangle class """
     def __init__(self, width, height, x=0, y=0, id=None):
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
-        self.id = id
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """ width getter """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ width setter """
+        self.setter_validator("width", value)
+        self.__width = value
+
+    @property
+    def height(self):
+        """ height getter """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ height setter """
+        self.setter_validator("height", value)
+        self.__height = value
+
+    @property
+    def x(self):
+        """ x getter """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """ x setter """
+        self.setter_validator("x", value)
+        self.__x = value
+
+    @property
+    def y(self):
+        """ y getter """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """ y setter """
+        self.setter_validator("y", value)
+        self.__y = value
+
+
+    @staticmethod
+    def setter_validator(attr, value):
+        """ validates the value inputs into the attributes """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(attr))
+        if attr == "x" or attr == "y":
+            if value < 0:
+                raise ValueError("{} must be > 0".format(attr))
+        elif value <= 0:
+            raise ValueError("{} must be > 0".format(attr))
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,80 @@
+#!usr/bin/python3
+""" Unittest module for rectangle module """
+import unittest
+from models.rectangle import Rectangle
+
+
+class TestRectangle(unittest.TestCase):
+    """ TestCases for Rectangle """
+    def test_id(self):
+        """ test rectangle """
+        r1 = Rectangle(1, 2, 4, 3, 5)
+        self.assertAlmostEqual(r1.id, 5)
+        r2 = Rectangle(1, 2)
+        self.assertAlmostEqual(r2.id, 1)
+        r3 = Rectangle(10, 2)
+        self.assertAlmostEqual(r3.id, 2)
+
+    def test_type_validation(self):
+        """ test for exception raised for wrong types """
+        with self.assertRaises(TypeError):
+            Rectangle("2", 10)
+        with self.assertRaises(TypeError):
+            Rectangle(None, 1)
+        with self.assertRaises(TypeError):
+            Rectangle((1, 2), 1)
+        with self.assertRaises(TypeError):
+            Rectangle({1: 2}, 2)
+        with self.assertRaises(TypeError):
+            Rectangle(4.5, 1)
+        with self.assertRaises(TypeError):
+            Rectangle(float('inf'), 1)
+
+        with self.assertRaises(TypeError):
+            Rectangle(2, "10")
+        with self.assertRaises(TypeError):
+            Rectangle(1, None)
+        with self.assertRaises(TypeError):
+            Rectangle(1, (1, 2))
+        with self.assertRaises(TypeError):
+            Rectangle(1, {1: 2})
+        with self.assertRaises(TypeError):
+            Rectangle(1, 4.5)
+        with self.assertRaises(TypeError):
+            Rectangle(1, float('inf'))
+        r = Rectangle(1, 2)
+        with self.assertRaises(TypeError):
+            r.width = "str"
+        with self.assertRaises(TypeError):
+            r.height = "str"
+        r = Rectangle(1, 2, 0, 0, 4)
+        with self.assertRaises(TypeError):
+            r.x = "4"
+        with self.assertRaises(TypeError):
+            r.y = "3"
+
+    def test_value_validation(self):
+        """ tests the values passed to the attributes of rectangle """
+        with self.assertRaises(ValueError):
+            Rectangle(-1, 2)
+        with self.assertRaises(ValueError):
+            Rectangle(0, 3)
+        with self.assertRaises(ValueError):
+            Rectangle(1, -3)
+        with self.assertRaises(ValueError):
+            Rectangle(2, 0)
+        with self.assertRaises(ValueError):
+            Rectangle(1, 2, -1, 3)
+        with self.assertRaises(ValueError):
+            Rectangle(10, 3, 3, -7)
+        r = Rectangle(10, 2)
+        with self.assertRaises(ValueError):
+            r.width = -3
+        with self.assertRaises(ValueError):
+            r.height = -4
+        r = Rectangle(1, 20, 4, 3)
+        with self.assertRaises(ValueError):
+            r.x = -2
+        with self.assertRaises(ValueError):
+            r.y = -1
+
