22bf08d9f82806a4120466041d024ec352234fb0
Idowu-David <idowudavidodun@gmail.com>
2023-04-11 12:51:52 +0000

    add: value argument

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/* This script prints the first argument passed to it */
+
+const argv = process.argv;
+
+if (argv[2]) {
+  console.log(argv[2]);
+} else {
+  console.log('No argument');
+}
