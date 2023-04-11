a8c1f5addc0843d704e5a651f580e82d7bcdda67
Idowu-David <idowudavidodun@gmail.com>
2023-04-11 12:32:32 +0000

    add: 3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+/* This script prints 3 languages */
+
+const lang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < 3; i++) {
+  console.log(lang[i]);
+}
