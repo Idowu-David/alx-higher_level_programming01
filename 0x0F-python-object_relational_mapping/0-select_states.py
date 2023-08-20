3700e175ec81a58d1d365caa768b00f274370d0e
Idowu-David <idowudavidodun@gmail.com>
2023-08-20 01:40:18 +0100

    add: list states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" This script lists all states from the database `hbtn_0e_0_usa`:
+    => This script takes 3 arguments: mysql username, mysql password and database name.
+    => This script connects to a MySQL server running on localhost at port 3306
+    => Results are sorted in ascending order by states.id
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    pwd = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost",
+                         user=username,
+                         password=pwd,
+                         database=database)
+    cur = db.cursor()
+    query = "SELECT * FROM states ORDER BY states.id"
+    cur.execute(query)
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    db.close()
